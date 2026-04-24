183ac0a37c86babc042b32aaabfc02eec165ba47
motheosekano10 <motheosekano10@gmail.com>
2026-04-24 13:11:59 +0200

    Change currency symbol from $ to R in SaleAdapter

--- app/src/main/java/com/example/inventoryapp/SaleAdapter.java
+++ app/src/main/java/com/example/inventoryapp/SaleAdapter.java
@@ -11,7 +11,7 @@ public class SaleAdapter extends RecyclerView.Adapter<SaleAdapter.VH> {
     @Override public VH onCreateViewHolder(ViewGroup p, int v) { return new VH(LayoutInflater.from(p.getContext()).inflate(R.layout.item_sale, p, false)); }
     @Override public void onBindViewHolder(VH h, int pos) {
         Sale s = list.get(pos);
-        h.product.setText(s.productName); h.qty.setText("Qty: " + s.quantity); h.total.setText("$" + String.format("%.2f", s.total)); h.date.setText(s.date);
+        h.product.setText(s.productName); h.qty.setText("Qty: " + s.quantity); h.total.setText("R" + String.format("%.2f", s.total)); h.date.setText(s.date);
     }
     @Override public int getItemCount() { return list.size(); }
     class VH extends RecyclerView.ViewHolder {
