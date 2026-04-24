63a609c3f0e1165d1c70b809661a1a6bed77a4e5
motheosekano10 <motheosekano10@gmail.com>
2026-04-24 12:42:07 +0200

    Refactor onCreate method in MainActivity
    Refactored onCreate method for better readability and null safety.

--- app/src/main/java/com/example/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/inventoryapp/MainActivity.java
@@ -1,21 +1,40 @@
 package com.example.inventoryapp;
+
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    @Override protected void onCreate(Bundle s) {
-        super.onCreate(s); setContentView(R.layout.activity_main);
+    @Override 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
         BottomNavigationView nav = findViewById(R.id.bottomNav);
-        nav.setOnItemSelectedListener(item -> {
-            Fragment f = null;
-            if(item.getItemId() == R.id.navDash) f = new DashboardFragment();
-            else if(item.getItemId() == R.id.navInv) f = new InventoryFragment();
-            else if(item.getItemId() == R.id.navSales) f = new SalesFragment();
-            else if(item.getItemId() == R.id.navRep) f = new ReportsFragment();
-            getSupportFragmentManager().beginTransaction().replace(R.id.frame, f).commit();
-            return true;
-        });
-        nav.setSelectedItemId(R.id.navDash);
+        if (nav != null) {
+            nav.setOnItemSelectedListener(item -> {
+                Fragment f = null;
+                int id = item.getItemId();
+                if (id == R.id.navDash) {
+                    f = new DashboardFragment();
+                } else if (id == R.id.navInv) {
+                    f = new InventoryFragment();
+                } else if (id == R.id.navSales) {
+                    f = new SalesFragment();
+                } else if (id == R.id.navRep) {
+                    f = new ReportsFragment();
+                }
+                
+                if (f != null) {
+                    getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame, f)
+                        .commit();
+                }
+                return true;
+            });
+            nav.setSelectedItemId(R.id.navDash);
+        }
     }
 }
